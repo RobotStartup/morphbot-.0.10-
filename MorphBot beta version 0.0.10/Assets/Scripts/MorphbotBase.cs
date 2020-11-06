c646f64bccf7833e365bf0b2b95ab0f7b91f27b4
A. Chris Eslami <chriseslami@selfassemblingsystems.com>
2020-11-06 13:35:22 -0500

    morphbot base

---
+++ MorphBot beta version 0.0.10/Assets/Scripts/MorphbotBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Collections.Specialized;
+
+public class MorphbotBase : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject morphbotUnitPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
